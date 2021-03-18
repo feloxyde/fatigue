d3f5b35742de583ebfc8bac09a6c4312a7f4b89f
Felix Bertoni <felix.bertoni987@gmail.com>
2021-03-18 13:57:17 +0100

    Even more tests passing. Should consider to defer showtypes through messages instead of doing it Test. Also, could do same for values instead maybe, like passing a map of values instead of a direct string ?

--- tests/test_Test_automsg_fail_showtypes.cpp
+++ tests/test_Test_automsg_fail_showtypes.cpp
@@ -1,151 +1,132 @@
 #include "fatigue/TestDriver.hpp"
 #include <fatigue/Test.hpp>
+#include "fatigue/OstreamTestRunner.hpp"
 
 
 #include <cassert>
+#include <sstream>
 
 using namespace ftg;
 
 struct TestAutoMsgFail : public Test {
-    TestAutoMsgFail() : Test("TestAutoMsgFail"){}
+    TestAutoMsgFail(OstreamTestLogger const& logger, std::stringstream const& ss) : Test("TestAutoMsgFail"), otl(logger), ss(ss){}
 
     virtual void run(){
+        std::stringstream res;
+
         check_equal(10, 9);
-        
+         
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 1);
+        res << "(1) [ERROR] expected check_equal with 10 (int) and 9 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
+
         check_near_equal(20, 12, 2);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 2);
+        res << "(2) [ERROR] expected check_near_equal with 20 (int) and 12 (int) tolerance 2 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
+
         check_near_equal(7, 10, 2);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 3);
+        res << "(3) [ERROR] expected check_near_equal with 7 (int) and 10 (int) tolerance 2 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
         
         check_not_equal(12, 12);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 4);
+        res << "(4) [ERROR] expected check_not_equal with 12 (int) and 12 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
+
+        check_less_than(12, 10);        
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 5);
+        res << "(5) [ERROR] expected check_less_than with 12 (int) and 10 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
         
-        check_less_than(12, 10);
-        check_less_than(12, 12);
+        check_less_than(12, 12);   
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 6);
+        res << "(6) [ERROR] expected check_less_than with 12 (int) and 12 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
 
         check_less_equal(12, 10);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 7);
+        res << "(7) [ERROR] expected check_less_equal with 12 (int) and 10 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
         
         check_greater_than(8, 9);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 8);
+        res << "(8) [ERROR] expected check_greater_than with 8 (int) and 9 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
+        
         check_greater_than(10, 10);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 9);
+        res << "(9) [ERROR] expected check_greater_than with 10 (int) and 10 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
         
         check_greater_equal(7, 8);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 10);
+        res << "(10) [ERROR] expected check_greater_equal with 7 (int) and 8 (int) to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
 
         check_throw<int>([](){});
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 11);
+        res << "(11) [ERROR] expected check_throw exception int to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
 
         check_nothrow([](){throw 1;});
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 12);
+        res << "(12) [ERROR] expected check_nothrow to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
 
         check_true(false);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 13);
+        res << "(13) [ERROR] expected check_true to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
+
 
         check_false(true);
+        assert(!otl.passed());
+        assert(otl.m_checkPassed == 0);
+        assert(otl.m_checkFailed == 14);
+        res << "(14) [ERROR] expected check_false to succeed, but failed." << std::endl;
+        assert(ss.str() == res.str());
     }
 
+    std::stringstream const& ss;
+    OstreamTestLogger const& otl;
 };
 
 int main(){
 
-    TestAutoMsgFail t;
+    std::stringstream ss;
+    OstreamTestLogger otl(ss);
+    TestAutoMsgFail t(otl, ss);
+    t.setLogger(&otl);
     t.setShowTypes(true);    
     t.run();
-    assert(!t.passed());
-    
-    assert(t.log().size() == 14);
-    
-
-    LogMessage lm = t.log()[0];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 1);
-    assert(lm.tindex() == 1);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_equal with 10 (int) and 9 (int) to succeed, but failed.");
-
-    lm = t.log()[1];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 2);
-    assert(lm.tindex() == 2);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_near_equal with 20 (int) and 12 (int) tolerance 2 (int) to succeed, but failed.");
-
-    lm = t.log()[2];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 3);
-    assert(lm.tindex() == 3);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_near_equal with 7 (int) and 10 (int) tolerance 2 (int) to succeed, but failed.");
- 
-    lm = t.log()[3];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 4);
-    assert(lm.tindex() == 4);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_not_equal with 12 (int) and 12 (int) to succeed, but failed.");
- 
-    lm = t.log()[4];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 5);
-    assert(lm.tindex() == 5);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_less_than with 12 (int) and 10 (int) to succeed, but failed.");
- 
-    lm = t.log()[5];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 6);
-    assert(lm.tindex() == 6);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_less_than with 12 (int) and 12 (int) to succeed, but failed.");
- 
-    lm = t.log()[6];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 7);
-    assert(lm.tindex() == 7);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_less_equal with 12 (int) and 10 (int) to succeed, but failed.");
- 
-    lm = t.log()[7];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 8);
-    assert(lm.tindex() == 8);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_greater_than with 8 (int) and 9 (int) to succeed, but failed.");
-
-    lm = t.log()[8];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 9);
-    assert(lm.tindex() == 9);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_greater_than with 10 (int) and 10 (int) to succeed, but failed.");
- 
-    lm = t.log()[9];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 10);
-    assert(lm.tindex() == 10);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_greater_equal with 7 (int) and 8 (int) to succeed, but failed.");
- 
-    lm = t.log()[10];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 11);
-    assert(lm.tindex() == 11);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_throw exception int to succeed, but failed.");
- 
-    lm = t.log()[11];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 12);
-    assert(lm.tindex() == 12);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_nothrow to succeed, but failed.");
- 
-    lm = t.log()[12];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 13);
-    assert(lm.tindex() == 13);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_true to succeed, but failed.");
- 
-    lm = t.log()[13];
-    assert(!lm.isImportant());
-    assert(lm.sindex() == 14);
-    assert(lm.tindex() == 14);
-    assert(lm.mode() == MESSAGE_CHECK);
-    assert(lm.description() == "expected check_false to succeed, but failed.");
- 
-
+   
     return 0;
 }
--- tests/test_Test_automsg_pass.cpp
+++ tests/test_Test_automsg_pass.cpp
@@ -1,8 +1,10 @@
 #include "fatigue/TestDriver.hpp"
 #include <fatigue/Test.hpp>
+#include <fatigue/OstreamTestRunner.hpp>
 
 
 #include <cassert>
+#include <sstream>
 
 using namespace ftg;
 
@@ -12,56 +14,48 @@ struct TestAutoMsgPass : public Test {
     virtual void run(){
         check_equal(10, 10);
         
-        assert(passed());
         check_near_equal(10, 12, 2);
-        assert(passed());
         check_near_equal(10, 10, 2);
-        assert(passed());
         check_near_equal(12, 10, 2);
-        assert(passed());
         
         check_not_equal(10, 12);
-        assert(passed());
         
         check_less_than(8, 10);
-        assert(passed());
 
         check_less_equal(8, 10);
-        assert(passed());
         check_less_equal(10, 10);
-        assert(passed());
         
         check_greater_than(10, 9);
-        assert(passed());
         
         check_greater_equal(10, 8);
-        assert(passed());
         check_greater_equal(10, 10);
-        assert(passed());
 
         check_throw<int>([](){
             throw (int)1;
         });
-        assert(passed());
 
         check_nothrow([](){});
-        assert(passed());
 
         check_true(true);
-        assert(passed());
 
         check_false(false);
-        assert(passed());
     }
 
 };
 
 int main(){
 
+    
+    std::stringstream ss;
     TestAutoMsgPass t;
-    
+    OstreamTestLogger otl(ss);
+    t.setLogger(&otl);
+
     t.run();
-    assert(t.passed());
-    
+    assert(otl.passed());
+    assert(otl.m_checkPassed == 15);
+    assert(otl.m_checkFailed == 0);
+    assert(ss.str().empty());
+
     return 0;
 }
